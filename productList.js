93e2fb23d045fa7c2fdb07c6bd8e1b6c6139d8c3
RichardFebres <rf57@njit.edu>
2020-02-23 12:10:30 -0500

    Add mock database to productList.js and populate img folder with product images

---
+++ productList.js
@@ -0,0 +1,60 @@
+const mockData = [
+    {
+      title: "Retro Bred 4's",
+      image: "img/Bred_Retro4.jpg",
+      type: ['high-top', 'basketball'],
+      price: 250.00
+    },
+
+    {
+        title: "Killshots",
+        image: "img/Killshots.jpg",
+        type: ['low-top', 'walking'],
+        price: 90.00
+    },
+
+
+    {
+        title: "Air Hurache's",
+        image: "img/Air_Hurache.jpg",
+        type: ['high-top', 'running'],
+        price: 110.00
+    },
+
+    {
+        title: "LeBron 11 Silver-Wolf Grey",
+        image: "img/Silver_Wolf_Gray_Lebron11.jpg",
+        type: ['high-top', 'basketball'],
+        price: 81.00
+    },
+
+    {
+        title: "Puma Axelion Breathe",
+        image: "img/Puma_Axelion_Breathe.jpg",
+        type: ['low-top', 'running'],
+        price: 60.00
+    },
+
+    {
+        title: "Adidas Ultraboosts ATR",
+        image: "img/Adidas_Ultraboosts_ATR.jpg",
+        type: ['high-top', 'walking'],
+        price: 139.00
+    },
+
+    {
+        title: "Kevin Durant 11 Grey",
+        image: "img/Kevin_Durant_11_Grey.jpg",
+        type: ['high-top', 'basketball'],
+        price: 93.00
+    },
+
+    {
+        title: "Kyrie Three Samurai",
+        image: "img/Kyrie_Three_Samurai.jpg",
+        type: ['low-top', 'basketball'],
+        price: 120.00
+    },
+];
+    
+
